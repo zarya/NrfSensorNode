dd9b2e0be89f2c8a46b676cf4786a4141aa32f77
Rudy Hardeman <zarya@gigafreak.net>
2014-08-15 13:56:16 +0200

    Added AS3935 lib

--- install.sh
+++ install.sh
@@ -7,3 +7,9 @@ rm -rf lib/RF24Network/examples
 rm -rf lib/RF24/RPi
 rm -rf lib/RF24/tests
 rm -rf lib/RF24/examples
+mkdir temp
+cd temp
+git clone https://github.com/SloMusti/AS3935-Arduino-Library AS3935
+cp -R AS3935/AS3935 ../lib
+cd ..
+rm -rf temp 
